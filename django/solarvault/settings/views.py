25a87b988188d4d4d186769c1e8f78d8a6125ed7
Yannic Brose <yannic@yannic-brose.de>
2024-06-11 13:18:46 +0200

    settings page created and form added to select serial interface

--- django/solarvault/settings/views.py
+++ django/solarvault/settings/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import serial
+import serial.tools
+import serial.tools.list_ports
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello!")
+    if request.method == "POST":
+          return HttpResponse("Hello!")
+    
+    serial_devices = serial.tools.list_ports.comports()#serial.tools.list_ports.comports()
+    return render(request, "settings/settings.html", {
+        "devices": serial_devices
+    })
