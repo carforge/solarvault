f8c61660ff7eba6c2bb9e33f04d712de868853cf
Yannic Brose <yannic@yannic-brose.de>
2024-06-11 14:36:18 +0200

    settings and controls app handling device correctly

--- django/solarvault/control/views.py
+++ django/solarvault/control/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from common.device import psu_device
 
 # Create your views here.
+def index(request):
+    if request.method == "POST":
+        device = psu_device(request.POST["device"])
+        return render(request, "control/index.html", {
+        "device": device
+        })
+
+    return render(request, "control/index.html")
--- django/solarvault/settings/views.py
+++ django/solarvault/settings/views.py
@@ -1,21 +1,12 @@
-from django import forms
-from django.http import HttpResponse
 from django.shortcuts import render
 from common.device import psu_device
-import serial
 import serial.tools
 import serial.tools.list_ports
 
+DEVICE = psu_device()
 
 # Create your views here.
-def index(request):
-    if request.method == "POST":
-        if not request.POST["device"] == None:
-            device = psu_device(request.POST["device"])
-            return render(request, "settings/settings.html", {
-                "selected_device": device
-            })
-    
+def index(request):    
     serial_devices = serial.tools.list_ports.comports()
     return render(request, "settings/settings.html", {
         "devices": serial_devices
