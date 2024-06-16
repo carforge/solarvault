cbd6b05e56595c2d75735fb3c8293218fe0f3e75
Yannic Brose <yannic@yannic-brose.de>
2024-06-16 21:45:10 +0200

    minor fixes

--- django/solarvault/solarvault/urls.py
+++ django/solarvault/solarvault/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('settings/', include("settings.urls")),
+    path('', include("settings.urls")),
     path('control/', include("control.urls"))
 ]
