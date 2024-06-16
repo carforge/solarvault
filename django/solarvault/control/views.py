0038d5838f40535457976ef065c1ddb02458ea8b
Yannic Brose <yannic@yannic-brose.de>
2024-06-16 21:32:02 +0200

    working web-ui and URL API

--- django/solarvault/control/views.py
+++ django/solarvault/control/views.py
@@ -2,47 +2,61 @@ from django.conf import settings
 from django.shortcuts import render
 from hanmatek import HM3xxP
 
-set_dict = {}
-
 # Create your views here.
-def index(request):
-    if request.method == "POST":
-        psu = HM3xxP(settings.MY_PSU.interface)
-        set_dict.update({
-            "voltage":request.POST["voltage"], 
-            "current": request.POST["current"]
-            })
-
-        if "psu_output" in request.POST:
-            set_dict.update({
-            "output":True
+def psu_control(request):
+    if len(request.GET) > 0:
+        interface = request.GET['if']
+        try:
+            psu = HM3xxP(interface)
+        except Exception:
+            message = "Failed to initialise serial port. Choose another Serial Interface."
+            return render(request, "control/control.html", {
+                "message": message
             })
         else:
-            set_dict.update({
-            "output":False
+            # Check if voltage is a numerical value
+            try:
+                if 0 <= float(request.GET['v']) <= 32:
+                    voltage = request.GET['v']
+            except Exception:
+                psu.write("output", False)
+                message = "Voltage is not a numerical value or out of range."
+                return render(request, "control/control.html", {
+                    "message": message
+                })
+            # Check if current is a numerical value
+            try:
+                if 0 <= float(request.GET['c']) <= 5:
+                    current = request.GET['c']
+            except Exception:
+                psu.write("output", False)
+                message = "Current is not a numerical value."
+                return render(request, "control/control.html", {
+                    "message": message
+                })
+            # Check if output is a boolean value
+            if request.GET['o'].lower() == "true" or request.GET['o'].lower() == "false":
+                current = request.GET['o']
+            else:
+                psu.write("output", False)
+                message = "Output is not a boolean value."
+                return render(request, "control/control.html", {
+                    "message": message
+                })
+            
+            current = request.GET['c']
+            output = request.GET['o'].capitalize()
+            psu.write("voltage:set", voltage)
+            psu.write("current:set", current)
+            psu.write("output", output)
+            return render(request, "control/control.html", {
+                "interface": f"Interface: {interface}",
+                "voltage": f"Voltage: {voltage} [V]",
+                "current": f"Current: {current} [A]",
+                "output": f"Output ON: {output}"
             })
-        psu.write("voltage:set", set_dict["voltage"])
-        psu.write("current:set", set_dict["current"])
-        psu.write("output", set_dict["output"])
-
-        return render(request, "control/index.html", {
-            "psu": psu.info(),
-            "voltage": set_dict["voltage"],
-            "current": set_dict["current"],
-            "output": set_dict["output"]
-        })
-    
-    try:
-        psu = HM3xxP(settings.MY_PSU.interface)
-    except Exception:
-        message = "Failed to initialise serial port. Go to settings."
-        return render(request, "control/index.html", {
-            "message": message
-        })
     else:
-        return render(request, "control/index.html", {
-            "psu": psu.info(),
-            "voltage": psu.read("voltage"),
-            "current": psu.read("current"),
-            "output": psu.read("output")
+        message = "Nothing specified. Go to settings or use settings paramters in URL."
+        return render(request, "control/control.html", {
+            "message": message
         })
--- django/solarvault/settings/views.py
+++ django/solarvault/settings/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.conf import settings
 import serial.tools
 import serial.tools.list_ports
 
@@ -7,14 +6,7 @@ import serial.tools.list_ports
 # Create your views here.
 def index(request):
     serial_devices = serial.tools.list_ports.comports()
-    if request.method == "POST":
-        settings.MY_PSU.mod_if(request.POST["device"])
-        return render(request, "settings/settings.html", {
-            "selected_device": settings.MY_PSU,
-            "devices": serial_devices
-        })
     
     return render(request, "settings/settings.html", {
-        "selected_device": settings.MY_PSU,
         "devices": serial_devices
     })
